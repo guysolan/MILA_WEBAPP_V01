d3954640b037c8b70ff7a0ab2418841a8cbf1a48
Guy Solan <thetismedical@gmail.com>
2022-02-23 17:30:43 +0100

    push data in terminal

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ import * as maths from './maths.js'
 import * as diseases from './disease-graph.js'
 import sensorFn from "./sensorFn.js"
 
+
+//FROM_RESEARCH_HOW_MUCH_DATA_NEEDED_TO_GET_VITALS
+const dataRequirement = 3e6
+
 //SETUP//SETUP//SETUP//SETUP//SETUP//SETUP//SETUP//SETUP//SETUP//SETUP//
+const terminal = document.getElementById('terminal')
 const ellipse1 = document.getElementById("ellipse1")
 const ellipse2 = document.getElementById("ellipse2")
 const ellipse3 = document.getElementById("ellipse3")
-const audio_memory = document.getElementById('audio-memory')
-const video_memory = document.getElementById('video-memory')
-const total_memory = document.getElementById('total-memory')
 const CCTV_1 = new CCTV(ellipse1)
 const CCTV_2 = new CCTV(ellipse2)
 const CCTV_3 = new CCTV(ellipse3)
@@ -60,6 +62,12 @@ let phoneMessageShown = false;
 let CCTVMessageShown = false;
 let processMessageShown = 0;
 let dataStoredMessageShow = false
+
+// Track Console
+let old_video_call_class = me.personElement.classList.contains('video-call')
+let old_phone_call_class = me.personElement.classList.contains('phone-call')
+let old_CCTV_class = me.personElement.classList.contains('CCTV')
+
 
 //FUNCTIONS//FUNCTIONS//FUNCTIONS//FUNCTIONS//FUNCTIONS//FUNCTIONS//FUNCTIONS//FUNCTIONS//
 function morePeople(numberPeople) {
@@ -128,7 +136,7 @@ function lightIcon(person, icon_id, sensor_classname) {
 
 function showMessage(message) {
   pause = true;
-  if (message){
+  if (message) {
     console.log(message)
   }
   message.classList.remove('hidden')
@@ -140,8 +148,14 @@ function showMessage(message) {
 //MAIN FUNCTION//MAIN FUNCTION//MAIN FUNCTION//MAIN FUNCTION//MAIN FUNCTION//MAIN FUNCTION//MAIN FUNCTION//
 function update(time) {
 
+  const delta = time - lastTime
+
+  let audio_memory = document.getElementById('audio-memory')
+  let video_memory = document.getElementById('video-memory')
+  // let total_memory = document.getElementById('total-memory')
+
   if (lastTime != null && pause !== true) {
-    if (introCount % 500 == 0){
+    if (introCount % 500 == 0) {
       clock.passTime()
     }
 
@@ -153,36 +167,30 @@ function update(time) {
     } else if (introCount == startIntro + 1) {
       showMessage(introWords2)
 
-    }  else if (introCount === startLearning && processMessageShown == 0) {
+    } else if (introCount === startLearning && processMessageShown == 0) {
       showMessage(startLearning1)
 
-    } else if (introCount == startLearning+1 && processMessageShown == 1){
+    } else if (introCount == startLearning + 1 && processMessageShown == 1) {
       showMessage(startLearning2)
       me.phone.PROCESS_DATA()
       me.phone.showVitals()
       console.log(me.phone.drive.vitals)
 
-    } else if (introCount == startLearning+2 && processMessageShown == 2){
+    } else if (introCount == startLearning + 2 && processMessageShown == 2) {
       showMessage(startLearning4)
 
-    } else if (introCount == startLearning+3 && processMessageShown == 3){
+    } else if (introCount == startLearning + 3 && processMessageShown == 3) {
       showMessage(startLearning5)
 
-    } else if (introCount == startLearning+4 && processMessageShown == 4){
+    } else if (introCount == startLearning + 4 && processMessageShown == 4) {
       showMessage(startLearning6)
-      
+
     } else if (introCount == startAddingPeople) {
       showMessage(introMorePeople)
 
     } else if (introCount === startAddingPeople + 1) {
       morePeople(18)
-
     }
-    // if (videoMessageShown == false || phoneMessageShown == true || CCTVMessageShown == true) {
-    //   if(dataStoredMessageShow = true){
-    //     showMessage(dataStoredMessage)
-    //   }
-    // }
 
     if (videoMessageShown == false && me.personElement.classList.contains('video-call')) {
       showMessage(videoCallMessage)
@@ -196,15 +204,10 @@ function update(time) {
     if (CCTVMessageShown == false && me.personElement.classList.contains('CCTV')) {
       showMessage(CCTVMessage)
       CCTVMessageShown = true
-
     }
 
-    const delta = time - lastTime
 
-    // Change Background
-    const hue = parseFloat(
-      getComputedStyle(document.documentElement).getPropertyValue("--hue")
-    )
+
 
     diseases.updateGraph()
 
@@ -212,35 +215,36 @@ function update(time) {
       person.updatePosition(delta)
     })
 
-    const prefix = ['bytes', 'Kb', 'Mb', 'Gb']
 
-    let videoInMemory = me.phone.space('video')
-    let audioInMemory = me.phone.space('audio')
-    let totalInMemory = me.phone.space('total')
 
-    const video_e = Math.max(0, Math.trunc((Math.log(videoInMemory+0.001))/(Math.log(1000))))
-    const audio_e = Math.max(0, Math.trunc((Math.log(audioInMemory+0.001))/(Math.log(1000))))
-    const total_e = Math.max(0, Math.trunc((Math.log(totalInMemory+0.001))/(Math.log(1000))))
+    showData('video-call', [video_memory, audio_memory], ['video', 'audio'], ['Video', 'Audio'], ['video-memory', 'audio-memory'], old_video_call_class, 'video', true)
+    // showData('video-call',audio_memory,'audio','Audio','audio-memory',old_video_call_class,'video',true)
+    old_video_call_class = me.personElement.classList.contains('video-call')
 
-    videoInMemory = maths.twoDP(videoInMemory)
-    audioInMemory = maths.twoDP(audioInMemory)
-    totalInMemory = maths.twoDP(totalInMemory)
-    total_memory.innerText = `Total Data Stored: ${totalInMemory/(1000**total_e)} ${prefix[total_e]}`
-    video_memory.innerText = `Video Data Stored: ${videoInMemory/(1000**video_e)} ${prefix[video_e]}`
-    audio_memory.innerText = `Audio Data Stored: ${audioInMemory/(1000**audio_e)} ${prefix[audio_e]}`
+    showData('phone-call', [audio_memory], ['audio'], ['Audio'], ['audio-memory'], old_phone_call_class, 'phone', true)
+    old_phone_call_class = me.personElement.classList.contains('phone-call')
+
+    showData('CCTV', [video_memory], ['video'], ['Video'], ['video-memory'], old_CCTV_class, 'CCTV', false)
+    old_CCTV_class = me.personElement.classList.contains('CCTV')
+
+
 
 
     useCCTV(allCCTV, people)
-    //onVideoCall(people)
-    //onPhoneCall(people)
+    onVideoCall(people)
+    onPhoneCall(people)
 
     people.forEach((person) => {
-      if (person.phone.space('total') > 1e6) {
+      if (person.phone.space('total') > dataRequirement) {
         // if (processMessageShown == 0 && person == me) {
         //   showMessage(startLearning11)
         // }
         person.phone.PROCESS_DATA()
+        if (person==me){
+          // startVitalsSequence()
+        }
         person.phone.showVitals()
+        cloneBottomOfTerminal('vitals-wrap')
         console.log(person.phone.drive.vitals)
       }
     })
@@ -248,10 +252,6 @@ function update(time) {
     lightIcon(me, 'blue-spot', 'video-call')
     lightIcon(me, 'red-spot', 'CCTV')
     lightIcon(me, 'green-spot', 'phone-call')
-
-
-    document.documentElement.style.setProperty("--hue", hue + delta * 0.01)
-
 
   }
 
@@ -270,5 +270,124 @@ continueBtn.addEventListener('click', function unPause() {
     }
   })
 })
+
+function cloneBottomOfTerminal(element_id) {
+  let element = document.getElementById(element_id);
+  const clone_e = element.cloneNode(true)
+  element.removeAttribute('id', element_id)
+  clone_e.setAttribute('id', element_id)
+  terminal.prepend(clone_e);
+}
+
+function moveBottomOfTerminal(element_id) {
+  let element = document.getElementById(element_id);
+  terminal.prepend(element);
+}
+
+function convertToBits(dataType) {
+  let dataInMemory = me.phone.space(dataType)
+  const prefix = ['bytes', 'Kb', 'Mb', 'Gb']
+  let data_e = Math.max(0, Math.trunc((Math.log(dataInMemory + 0.001)) / (Math.log(1000))))
+  dataInMemory = maths.twoDP(dataInMemory)
+  let formatted = `${dataInMemory/(1000**data_e)} ${prefix[data_e]}`
+  return {
+    prefix,
+    data_e,
+    dataInMemory,
+    formatted
+  }
+}
+
+function showMemory(dataType, dataElem, dataName) {
+
+  const data = convertToBits(dataType)
+  dataElem.innerHTML = `Saving ${dataName} Data = <span class='green'>True</span>
+  <br><br>
+  ${dataName} Data Stored: ${data.formatted}
+  <br><br>
+  <div class='bar-chart' style='width:${data.dataInMemory/dataRequirement*90}%; max-width: 90%'></div>`
+}
+
+function showTotal() {
+  let totalMemoryElem = document.getElementById('total-memory')
+  let data = convertToBits('total')
+  totalMemoryElem.innerHTML = `
+  Data Requirement: ${maths.twoDP(data.dataInMemory/dataRequirement*100)}% 
+  <br><br>
+  <div class='bar-chart' style='width:${data.dataInMemory/dataRequirement*90}%; max-width: 90%'></div>`
+}
+
+function stopStreaming(dataElem, dataName) {
+  let stoppedStreaming = document.createElement('p')
+  stoppedStreaming.innerHTML = `Saving ${dataName} Data = <span class='red'>False</span>`
+  dataElem.appendChild(stoppedStreaming)
+}
+
+function showAction(dataElem, name, call = true) {
+  console.log('here')
+  let newAction = document.createElement('p')
+  if (call) {
+    newAction.innerHTML = `<br>
+    ---------------------------
+    <br>
+    | New ${name} call detected |
+    <br>
+    ---------------------------
+    `
+  } else if (call == false) {
+    newAction.innerHTML = `<br>
+    ---------------------------
+    <br>
+    | Detected in ${name} area |
+    <br>
+    ---------------------------`
+  }
+  dataElem.appendChild(newAction)
+}
+
+function startVitalsSequence() {
+  me.stop()
+  messageAfterDelay('Starting User Identification...',10)
+  messageAfterDelay('User Identified',1000)
+}
+
+function messageAfterDelay(words,time){
+  setTimeout(() => {
+    let message = document.createElement('p')
+    message.innerHTML = `
+  <br>
+  ${words}
+  <br>`
+    terminal.prepend(message)
+  }, time);
+}
+
+function showData(data_className = 'video-call', memory_type, dataType = 'video', dataName = 'Video', element_id = 'video-memory', old_data_class, name, call = true) {
+
+  let dataClass = me.personElement.classList.contains(data_className)
+
+  if (old_data_class == true) {
+
+    showTotal()
+    for (let i = 0; i < dataType.length; i++) {
+      showMemory(dataType[i], memory_type[i], dataName[i])
+    }
+  }
+
+  if (old_data_class == false && dataClass == true) {
+    cloneBottomOfTerminal('total-memory')
+
+    for (let i = 0; i < dataType.length; i++) {
+      cloneBottomOfTerminal(element_id[i])
+    }
+    showAction(memory_type[0], name, call)
+
+  } else if (old_data_class == true && dataClass == false) {
+    for (let i = 0; i < dataType.length; i++) {
+      stopStreaming(memory_type[i], dataName[i])
+
+    }
+  }
+}
 
 window.requestAnimationFrame(update)
